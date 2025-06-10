9918b957835d8cc3433205ccd8bf8544e869f52e
Anvar Daulbaev <av.daulbaev@gmail.com>
2025-06-10 18:53:59 +0600

    First comit: Installed the necessary dependencies

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+const Footer = ({}) => {
+  return (
+    <footer className={styles.footer} />
+  )
+};
+
+export default Footer;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+const Header = ({}) => {
+  return (
+    <header className={styles.header} />
+  )
+};
+
+export default Header;
