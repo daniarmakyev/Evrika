350d9752daeea28502adfbf953555d2d243093f5
Daniar Makyev <daniarmakyev10@gmail.com>
2025-06-12 14:40:19 +0600

    зарефакторил код исправил визуальный баг оверлэя

--- src/components/Ui/FormButton/index.tsx
+++ src/components/Ui/FormButton/index.tsx
@@ -1,31 +1,56 @@
-import React from "react";
-import styles from "./styles.module.scss";
+import React, { ButtonHTMLAttributes, forwardRef } from "react";
+import styles from "./style.module.scss";
 
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: "primary" | "secondary" | "outline";
-  size?: "small" | "medium" | "large";
+type ButtonType = "button" | "submit" | "reset";
+
+interface ButtonProps extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, "style" | "className" | "type"> {
   fullWidth?: boolean;
+  style?: React.CSSProperties;
   children: React.ReactNode;
+  disabled?: boolean;
+  type?: ButtonType;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  onFocus?: (event: React.FocusEvent<HTMLButtonElement>) => void;
 }
 
-const Button: React.FC<ButtonProps> = ({
-  variant = "primary",
-  size = "medium",
-  fullWidth = false,
-  children,
-  className,
-  ...props
-}) => {
-  return (
-    <button
-      className={`${styles.button} ${styles[variant]} ${styles[size]} ${
-        fullWidth ? styles.fullWidth : ""
-      } ${className || ""}`}
-      {...props}
-    >
-      {children}
-    </button>
-  );
-};
+const FormButton = forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    {
+      fullWidth = false,
+      style = {},
+      children,
+      disabled = false,
+      type = "button",
+      onClick,
+      onFocus,
+      ...rest
+    },
+    ref
+  ) => {
+    const buttonStyle = {
+      ...style,
+      width: fullWidth ? "100%" : undefined,
+      opacity: disabled ? 0.6 : 1,
+      cursor: disabled ? "not-allowed" : "pointer",
+    };
 
-export default Button;
+    return (
+      <button
+        ref={ref}
+        style={buttonStyle}
+        type={type}
+        disabled={disabled}
+        className={styles.button}
+        onClick={onClick}
+        onFocus={onFocus}
+        {...rest}
+      >
+        {children}
+      </button>
+    );
+  }
+);
+
+FormButton.displayName = "FormButton";
+
+export default FormButton; 
