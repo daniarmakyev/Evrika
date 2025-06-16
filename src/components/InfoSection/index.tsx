b2c2eedc1543af18215ea248aa2700fa2b1ecaad
Anvar Daulbaev <av.daulbaev@gmail.com>
2025-06-16 17:43:53 +0600

    Added Info Section Name, added color in varibles

---
+++ src/components/InfoSection/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import styles from "./styles.module.scss";
+import Link from "next/link";
+
+interface Inormation {
+  title: string;
+  description: string;
+  all_see?: string;
+  link?: string;
+}
+
+const InfoSection: React.FC<Inormation> = ({title, description, all_see, link}) => {
+  return (
+    <div className={styles.info}>
+      <div className={styles.info__title}>
+        <h3>{title}</h3>
+      </div>
+      <div className={styles.info__details}>
+        <div className={styles.info__description}>
+          <p>{description}</p>
+        </div>
+        {all_see && link && (
+          <div className={styles.info__allSee}>
+            <Link href={link}>
+              {all_see}
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default InfoSection;
