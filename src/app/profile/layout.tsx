14cef45aca68bc0202004ec0532dff911b02e0d5
Daniar Makyev <daniarmakyev10@gmail.com>
2025-07-25 08:25:06 +0600

    fix

--- src/app/profile/layout.tsx
+++ src/app/profile/layout.tsx
@@ -33,7 +33,7 @@ export default function ProfileLayout({
     }
   }, [params,dispatch]);
 
-  // const isProfileStudent = !!studentId;
+  const isProfileStudent = !!studentId;
 
   useEffect(() => {
     dispatch(getUser());
@@ -69,7 +69,7 @@ export default function ProfileLayout({
             <StudentTabBar
               role={user.role}
               studentId={studentId}
-              // isProfileStudent={false}
+              isProfileStudent={isProfileStudent}
             />
           </>
         ) : (
